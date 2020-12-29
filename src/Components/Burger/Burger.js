b0fd16cbb38a4e1f9bfb9aeccc8c8a52f5f991f7
Ismaeel Yaghoubi <e30.sa23@gmail.com>
2020-12-29 20:11:04 +0100

    added the state and ingredient dynamically

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,13 +2,21 @@ import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () =>{
+const burger = (props) =>{
+    // const TreansformedeIngredients = Object.keys(props.ingredients)
+    // .map(ingredient => {
+    //     return [...Array(props.ingredients[ingredient])].map((_, i) =>{
+    //         return <BurgerIngredient key={ingredient + i} type={ingredient}/>
+    //     })
+    // })
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
+            {Object.keys(props.ingredients).map(ingredient =>{
+                return [...Array(props.ingredients[ingredient])].map((_, i) =>{
+                    return <BurgerIngredient key={ingredient + i} type={ingredient} />
+                })
+            })}
             <BurgerIngredient type="bread-bottom"/>
         </div>
 
